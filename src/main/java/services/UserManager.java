a633201a81a96903c223da9dd71b0f7be0fbccd6
BoriNedeva <bori_ned94@abv.bg>
2017-01-03 23:12:32 +0200

    Headers to allow CORS

--- src/main/java/services/UserManager.java
+++ src/main/java/services/UserManager.java
@@ -1,8 +1,10 @@
 package services;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.OPTIONS;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -16,11 +18,20 @@ public class UserManager {
 	@POST
 	@Path("login")
 	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response login(LoginUser user){
 		//{ "loginUser" : {"username" : "rest1", "password" : "rest1"}} - sample request
 		Gson gson = new Gson();
 		System.out.println(user.getPassword());
 		System.out.println(user.getUsername());
-		return Response.status(Response.Status.OK).entity(gson.toJson(new String("OK!!!"))).build();
+		return Response.status(Response.Status.OK).entity(gson.toJson(user)).header("Access-Control-Allow-Origin", "*").header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS").header("Access-Control-Allow-Headers", "Content-Type,Accept").build();
+	}
+	
+	@OPTIONS
+	@Path("login")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response loginPre(){
+		return Response.status(Response.Status.OK).header("Access-Control-Allow-Origin", "*").header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS").header("Access-Control-Allow-Headers", "Content-Type,Accept").build();
 	}
 }
